7d92558ab0a078dac8433f4e848cf058ab7d0e79
Thinh Ho <hoquocthinh102@gmail.com>
2025-12-24 00:57:27 -0500

    fix base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/noel-cho-be=thu-official/",
+  base: "/noel-cho-be-thu-official/",
   plugins: [react()],
 });
